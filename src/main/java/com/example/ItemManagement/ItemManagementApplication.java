7e82739384086563bd73e338ac33f8c615a80dfb
zerkodv <s3937120@student.rmit.edu.au>
2022-09-08 19:56:32 +1000

    comment
    just a comment

--- src/main/java/com/example/ItemManagement/ItemManagementApplication.java
+++ src/main/java/com/example/ItemManagement/ItemManagementApplication.java
@@ -11,7 +11,7 @@ import java.util.List;
 @SpringBootApplication
 
 public class ItemManagementApplication {
-
+	//main app
 	public static void main(String[] args) {
 
 		SpringApplication.run(ItemManagementApplication.class, args);
