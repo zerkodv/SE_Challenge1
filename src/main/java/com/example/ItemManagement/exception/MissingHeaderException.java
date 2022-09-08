99442f9dde98fa81d4ea3e17e6e6d7fb25e14ed0
zerkodv <s3937120@student.rmit.edu.au>
2022-09-08 23:23:04 +1000

    added exception handlers
    added exception handlers for  missing header, rensponse entity and error response

---
+++ src/main/java/com/example/ItemManagement/exception/MissingHeaderException.java
@@ -0,0 +1,9 @@
+package com.example.ItemManagement.exception;
+
+public class MissingHeaderException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public MissingHeaderException(String exception) {
+        super(exception);
+    }
+}
